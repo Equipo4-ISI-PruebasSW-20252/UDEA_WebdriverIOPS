472bc1eafa55cab8b7817b9e781979c176bfb6ec
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-11-07 17:13:34 -0500

    Se actualiza el step de status

--- features/step-definitions/status.steps.js
+++ features/step-definitions/status.steps.js
@@ -9,8 +9,17 @@ const pages = {
 
 Then(/^I see all my accounts in a table$/, async () => {
     await pages.status.accountsTable.waitForDisplayed({ timeout: 5000 });
+
+    await browser.waitUntil(async () => {
+        const rows = await pages.status.rows;
+        return rows.length > 0;
+    }, {
+        timeout: 5000,
+        timeoutMsg: 'No se encontraron filas en la tabla de cuentas'
+    });
+
     const rows = await pages.status.rows;
     console.log('Número de filas:', rows.length);
 
     expect(rows.length).toBeGreaterThan(0);
-})
+});
