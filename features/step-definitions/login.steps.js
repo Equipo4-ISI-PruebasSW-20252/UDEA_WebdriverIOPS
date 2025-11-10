e3d6569eae417e102cfc5aec6dd27bf4c23a452c
cristianmunoz1 <cristian.munoz3@udea.edu.co>
2025-11-10 10:21:46 -0500

    Se arregla los steps de login

--- features/step-definitions/login.steps.js
+++ features/step-definitions/login.steps.js
@@ -3,15 +3,15 @@ import pages from "../pageobjects/pages.js";
 
 //LOGIN
 When(/^I login with (\w+) and (.+)$/, async (username, password) => {
-  await pages.LoginPage.login(username, password);
+  await pages.login.login(username, password);
 });
 
 //Disabled Log In button
 Then(/^the login button should be disabled$/, async () => {
-  const isEnabled = await pages.LoginPage.btnSubmit.isEnabled();
+  const isEnabled = await pages.login.btnSubmit.isEnabled();
   await expect(isEnabled).toBe(false);
 });
 
 Given(/^I am logged in correctly$/, async () => {
-  await pages.LoginPage.login("john", "demo");
+  await pages.login.login("john", "demo");
 });
