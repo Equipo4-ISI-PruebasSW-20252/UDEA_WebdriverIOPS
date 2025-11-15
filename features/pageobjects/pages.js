729e46590c7b249374fbd6436bcff3448ac2839d
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-15 17:42:21 -0500

    Add transferFundsPage to pages object

--- features/pageobjects/pages.js
+++ features/pageobjects/pages.js
@@ -1,11 +1,13 @@
 import LoginPage from "../pageobjects/login.page.js";
 import statusPage from "../pageobjects/status.page.js";
 import accountPage from "../pageobjects/account.page.js";
+import transferFundsPage from "../pageobjects/transfer.page.js";
 
 const pages = {
   login: LoginPage,
   status: statusPage,
   account: accountPage,
+  transferFunds: transferFundsPage,
 };
 
 export default pages;
