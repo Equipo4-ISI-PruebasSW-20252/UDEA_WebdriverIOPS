1194e1f165626b2412c58f4a74022565b6ee721f
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-15 17:37:54 -0500

    Add step definitions for transfer funds feature

---
+++ features/step-definitions/transfer.steps.js
@@ -0,0 +1,23 @@
+import { When, Then } from "@wdio/cucumber-framework";
+import pages from "../pageobjects/pages.js";
+
+When(/^I navigate to the Transfer Funds page$/, async () => {
+    await pages.transferFunds.openTransferPage();
+});
+
+When(/^I transfer (\d+) from the first account to the second account$/, async (amount) => {
+  
+    const fromOptions = await pages.transferFunds.fromAccountDropdown.$$('option');
+    
+    await expect(fromOptions.length).toBeGreaterThanOrEqual(2);
+    
+    const fromAccountText = await fromOptions[0].getText();
+    const toAccountText = await fromOptions[1].getText();
+
+    await pages.transferFunds.transfer(amount, fromAccountText, toAccountText);
+});
+
+Then(/^I should see the transfer confirmation message$/, async () => {
+    await expect(pages.transferFunds.successMessage).toBeExisting();
+    await expect(pages.transferFunds.successMessage).toHaveTextContaining('Transfer Complete!');
+});
