420725ca6c553e71b0f36a197b7318138bad571a
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-15 20:25:26 -0500

    Update transfer.steps.js

--- features/step-definitions/transfer.steps.js
+++ features/step-definitions/transfer.steps.js
@@ -6,18 +6,18 @@ When(/^I navigate to the Transfer Funds page$/, async () => {
 });
 
 When(/^I transfer (\d+) from the first account to the second account$/, async (amount) => {
-  
+    
+    await pages.transferFunds.fromAccountDropdown.$('option:nth-child(2)').waitForExist({ 
+        timeout: 10000, 
+        message: 'No se encontraron suficientes cuentas de origen para la transferencia.'
+    });
+
     const fromOptions = await pages.transferFunds.fromAccountDropdown.$$('option');
     
-    await expect(fromOptions.length).toBeGreaterThanOrEqual(3);
+    await expect(fromOptions.length).toBeGreaterThanOrEqual(3); 
     
     const fromAccountText = await fromOptions[1].getText();
     const toAccountText = await fromOptions[2].getText();
 
     await pages.transferFunds.transfer(amount, fromAccountText, toAccountText);
-});
-
-Then(/^I should see the transfer confirmation message$/, async () => {
-    await expect(pages.transferFunds.successMessage).toBeExisting();
-    await expect(pages.transferFunds.successMessage).toHaveTextContaining('Transfer Complete!');
 });
