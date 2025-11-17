0e18feabea1f4cc7837a9ab478065ca261f043f6
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-17 15:36:58 -0500

    Refactor loanStatus selector for improved accuracy

--- features/pageobjects/loan.page.js
+++ features/pageobjects/loan.page.js
@@ -22,7 +22,7 @@ class LoanPage extends Page {
     }
 
     get loanStatus() {
-        return $("//tr[./td[contains(., 'Loan Status:')]]/td[2]/*[@id='loanStatus']"); 
+        return $("//td[./b[normalize-space()='Status:']]/following-sibling::td[@id='loanStatus']"); 
     }
 
     async requestLoan(amount, downPayment) {
