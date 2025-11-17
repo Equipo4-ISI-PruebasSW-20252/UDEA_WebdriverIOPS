10d24db21dde56e7ee0d2ac62f2a1b2d4f140c1c
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-16 22:33:07 -0500

    Create LoanPage class for loan request functionality

---
+++ features/pageobjects/loan.page.js
@@ -0,0 +1,47 @@
+import Page from "./page.js";
+
+class LoanPage extends Page {
+    get inputLoanAmount() {
+        return $("//input[@id='amount']");
+    }
+
+    get inputDownPayment() {
+        return $("//input[@id='downPayment']");
+    }
+
+    get selectFromAccount() {
+        return $("//select[@id='fromAccountId']");
+    }
+
+    get btnApplyNow() {
+        return $("//input[@value='Apply Now']");
+    }
+    
+    get titleConfirmation() {
+        return $("//h1[normalize-space()='Loan Request Processed']");
+    }
+
+    get loanStatus() {
+        return $("//span[@id='loanStatus']");
+    }
+
+    async requestLoan(amount, downPayment) {
+        await this.inputLoanAmount.setValue(amount);
+        await this.inputDownPayment.setValue(downPayment);
+    }
+
+    async selectFromAccountOption(index) {
+        await this.selectFromAccount.selectByIndex(index);
+    }
+
+    async getLoanStatus() {
+        await this.loanStatus.waitForDisplayed({ timeout: 5000 });
+        return await this.loanStatus.getText();
+    }
+
+    open() {
+        return super.open('requestloan');
+    }
+}
+
+export default new LoanPage();
