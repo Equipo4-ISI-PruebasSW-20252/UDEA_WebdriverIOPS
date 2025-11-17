de4c2c46ef3327edbdae3ef9190ca1fb8e15af86
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-16 22:41:46 -0500

    Add LoanPage to page objects

--- features/pageobjects/pages.js
+++ features/pageobjects/pages.js
@@ -2,12 +2,14 @@ import LoginPage from "../pageobjects/login.page.js";
 import statusPage from "../pageobjects/status.page.js";
 import accountPage from "../pageobjects/account.page.js";
 import transferFundsPage from "../pageobjects/transfer.page.js";
+import LoanPage from '../pageobjects/loan.page.js'
 
 const pages = {
   login: LoginPage,
   status: statusPage,
   account: accountPage,
   transferFunds: transferFundsPage,
+  loan: LoanPage,
 };
 
 export default pages;
