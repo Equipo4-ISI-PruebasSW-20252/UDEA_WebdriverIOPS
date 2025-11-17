e768c61e51e5f7a2753920cb8e3a53de6a0ec6aa
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-16 22:40:09 -0500

    Add loan request step definitions for Cucumber tests

---
+++ features/step-definitions/loan.steps.js
@@ -0,0 +1,34 @@
+import { Given, When, Then } from "@wdio/cucumber-framework";
+import pages from "../pageobjects/pages.js";
+
+// GIVEN
+
+Given('I am on the Request Loan page', async () => {
+    await pages.loan.open();
+    await pages.loan.inputLoanAmount.waitForDisplayed();
+});
+
+// WHEN
+
+When(/^I request a loan for (\d+) with down payment of (\d+)$/, async (amount, downPayment) => {
+    await pages.loan.requestLoan(amount, downPayment);
+});
+
+When('I select the first account for fund deposit', async () => {
+    await pages.loan.selectFromAccountOption(0);
+});
+
+When('I click the apply now button', async () => {
+    await pages.loan.btnApplyNow.click();
+});
+
+// THEN
+
+Then(/^I should see a loan confirmation message saying "([^"]*)"$/, async (message) => {
+    await expect(pages.loan.titleConfirmation).toBeExisting();
+    await expect(pages.loan.titleConfirmation).toHaveText(message);
+});
+
+Then(/^the loan status should be "([^"]*)"$/, async (expectedStatus) => {
+    await expect(pages.loan.loanStatus).toHaveText(expectedStatus);
+});
