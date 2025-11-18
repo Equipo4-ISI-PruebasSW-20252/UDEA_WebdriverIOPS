fa25299a89d2d24146ad66b54634a096d909d3e8
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-17 20:58:08 -0500

    Update transfer error message validation

--- features/step-definitions/transfer.steps.js
+++ features/step-definitions/transfer.steps.js
@@ -34,15 +34,15 @@ Then(/^I should see the transfer confirmation message$/, async () => {
 });
 
 Then(/^I should see the transfer failure message$/, async () => {
-    const expectedErrorText = 'An internal error has occurred and has been logged.'; 
+    const expectedDetailText = 'An internal error has occurred and has been logged.';
     
-    await pages.transferFunds.transferErrorText.waitForDisplayed({ 
-        timeout: 20000, 
-        timeoutMsg: 'El mensaje de error de transferencia no se mostró.' 
+    await pages.transferFunds.errorHeader.waitForDisplayed({ 
+        timeout: 10000, 
+        timeoutMsg: 'El encabezado "Error!" no apareció en la página de transferencia.' 
     });
 
     await expect(pages.transferFunds.transferErrorText).toBeExisting();
     await expect(pages.transferFunds.transferErrorText).toHaveText(
-        expect.stringContaining(expectedErrorText)
+        expect.stringContaining(expectedDetailText)
     );
 });
