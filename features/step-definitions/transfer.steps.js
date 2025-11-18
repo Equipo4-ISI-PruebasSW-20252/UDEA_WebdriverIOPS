33467b2aa4f4562d55413f00d404397b3a9a3a83
Huber Steven Arroyave Rojas <114766708+hubersteven@users.noreply.github.com>
2025-11-18 17:28:31 -0500

    Reduce timeout for transfer failure message

--- features/step-definitions/transfer.steps.js
+++ features/step-definitions/transfer.steps.js
@@ -37,7 +37,7 @@ Then(/^I should see the transfer failure message$/, async () => {
     const expectedDetailText = 'An internal error has occurred and has been logged.';
     
     await pages.transferFunds.errorHeader.waitForDisplayed({ 
-        timeout: 60000, 
+        timeout: 50000, 
         timeoutMsg: 'El encabezado "Error!" no apareció en la página de transferencia.' 
     });
 
